966d116bf3bcdb67c15d1b32f089a3676d488c74
Cristian Da Conceicao <cristiandaconceicao12@gmail.com>
2023-10-20 16:02:58 -0500

    Add Add To Order

--- src/components/CheckoutSideMenu.jsx
+++ src/components/CheckoutSideMenu.jsx
@@ -14,6 +14,18 @@ function CheckoutSideMenu() {
     context.setCartProducts(filteredProducts);
   };
 
+  const handleCheckout = () => {
+    const orderToAdd = {
+      date: "01.02.23",
+      products: context.cartProducts,
+      totalProducts: context.cartProducts.lenght,
+      totalPrice: totalPrice(context.cartProducts),
+    };
+
+    context.setOrder([...context.order, orderToAdd]);
+    context.setCartProducts([]);
+  };
+
   return (
     <aside
       style={{
@@ -34,7 +46,7 @@ function CheckoutSideMenu() {
           <XMarkIcon className="h-6 w-6 text-black"></XMarkIcon>
         </div>
       </div>
-      <div className="px-6 overflow-y-scroll">
+      <div className="px-6 overflow-y-scroll flex-1">
         {context.cartProducts.map((product) => (
           <OrderCard
             key={product.id}
@@ -46,13 +58,19 @@ function CheckoutSideMenu() {
           />
         ))}
       </div>
-      <div className="px-6">
+      <div className="px-6 mb-10">
         <p className="flex justify-between items-center">
           <span className="font-light">Total:</span>
           <span className="font-medium text-2xl">
             ${totalPrice(context.cartProducts)}
           </span>
         </p>
+        <button
+          className="bg-black py-3 text-white w-full rounded-lg"
+          onClick={() => handleCheckout()}
+        >
+          Checkout
+        </button>
       </div>
     </aside>
   );
